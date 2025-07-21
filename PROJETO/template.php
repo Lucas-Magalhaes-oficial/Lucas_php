78e6f583d84ec2747688c0c330b7cf324321960d
Lucas-Magalhaes-oficial <lucas.magalhaes.lms@gmail.com>
2025-07-21 16:53:39 -0300

    update nos arquivos tarefas e template
    arquivo tareFA (no singular), foi transformado em inicio.php, por favor ignorar esse inicio.php, para evitar confusão

--- PROJETO/template.php
+++ PROJETO/template.php
@@ -14,8 +14,31 @@
                 Tarefa:
                 <input type="text" name="nome"/>
             </label>
-            <input type="submit" value="cadastrar"/>
+            <label>
+                Descrição (opcional):
+                <textarea name="descricao"></textarea>
+            </label>
+            <label>
+                Prazo (opcional):
+                <input type="text" name="prazo">
+            </label>
         </fieldset>
+        <fieldset>
+            <legend>Prioridade:</legend>
+            <label>
+                <input type="radio" name="prioridade" value="baixa" checked>
+                Baixa
+                <input type="radio" name="prioridade" value="media">
+                Média
+                <input type="radio" name="prioridade" value="alta">
+                Alta
+            </label>
+        </fieldset>
+        <label>
+            Tarefa concluida:
+            <input type="checkbox" name="concluida" value="sim">
+        </label>
+            <input type="submit" value="Cadastrar">
     </form>
     <table>
         <tr>
@@ -23,11 +46,16 @@
             <th>Descricao</th>
             <th>Prazo</th>
             <th>Prioridade</th>
-            <th>concluida</th>
+            <th>Concluida</th>
         </tr>
-        <?php foreach ($lista_tarefas as $tarefa) : ?>
+        
+        <?php foreach($lista_tarefas as $tarefa) : ?>
             <tr>
-                <td><?php echo $tarefa; ?></td>
+                <td><?php echo $tarefa['nome']; ?></td>
+                <td><?php echo $tarefa['descricao']; ?></td>
+                <td><?php echo $tarefa['prazo']; ?></td>
+                <td><?php echo $tarefa['prioridade']; ?></td>
+                <td><?php echo $tarefa['concluida']; ?></td>
             </tr>
         <?php endforeach; ?>
     </table>
