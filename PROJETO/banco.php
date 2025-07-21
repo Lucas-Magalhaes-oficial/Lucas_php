d601b117d95450157f80d8fe115a8b1cb12eab35
Lucas-Magalhaes-oficial <lucas.magalhaes.lms@gmail.com>
2025-07-21 17:03:22 -0300

    criação da conexão com banco
    aqui temos uma basica conexão de banco

---
+++ PROJETO/banco.php
@@ -0,0 +1,11 @@
+<?php
+    $bdServidor = '127.0.0.1';
+    $bdUsuario = 'root';
+    $bdSenha = 'root';
+    $bdBanco = 'lucas_magalhaes';
+    $conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
+        if (mysqli_connect_errno($conexao)) {
+            echo "Problemas para conectar no banco. Verifique os dados informados.";
+            die();
+        }
+?>
