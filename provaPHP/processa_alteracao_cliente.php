415de236010fbadf8da1469607ed2a89054a6242
Lucas-Magalhaes-oficial <lucas.magalhaes.lms@gmail.com>
2025-08-21 22:12:26 -0300

    resolução do problema
    aquela tela

--- provaPHP/processa_alteracao_cliente.php
+++ provaPHP/processa_alteracao_cliente.php
@@ -9,9 +9,9 @@ if($_SESSION['perfil'] !=1){
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $id_cliente = $_POST['id_cliente'];
-    $nome = $_POST['nome'];
+    $nome_cliente = $_POST['nome'];
     $email = $_POST['email'];
-    $endereco = $_POST['email'];
+    $endereco = $_POST['endereco'];
     $telefone = $_POST['telefone'];
     $nova_senha = !empty($_POST['nova_senha'])? password_hash($_POST['nova_senha'],PASSWORD_DEFAULT) : null;
 
@@ -24,17 +24,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $sql="UPDATE cliente SET nome_cliente=:nome_cliente,email=:email,endereco=:endereco,telefone=:telefone WHERE id_cliente = :id";
         $stmt = $pdo->prepare($sql);
     }
-    $stmt->bindParam(':nome',$nome);
+    $stmt->bindParam(':nome_cliente',$nome_cliente);
     $stmt->bindParam(':email',$email);
     $stmt->bindParam(':endereco',$endereco);
     $stmt->bindParam(':telefone',$telefone);
+    $stmt->bindParam(':id', $id_cliente);
     
     
 
     if($stmt->execute()){
         echo "<script>alert('Cliente atualizado com sucesso!');window.location.href='buscar_cliente.php';</script>";
     }else{
-        echo "<script>alert('Erro ao atualizar cliente');window.location.href='alterar_cliente.php?id=$id_cliente';</script>";
+        echo "<script>alert('Erro ao atualizara cliente');window.location.href='alterar_cliente.php?id=$id_cliente';</script>";
     }
 }
 ?>
